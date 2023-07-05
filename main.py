a2bb9bfa856ac6801d53153eee97b6b021f2a714
rationalf <iamsergeydzyuba@mail.ru>
2023-07-06 01:51:10 +0300

    rework view of card

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ class ResultOfGame:
         resize_keyboard(id_of_user)
         get_player(id_of_user).set_currency_for_player()
 
-
     def loser(self, id_of_user):
         telegram_bot.send_message(id_of_user, "=========================")
         telegram_bot.send_message(id_of_user, f'<b>You Lose!</b>',
@@ -55,30 +54,114 @@ def resize_keyboard(id_of_user):
 
 
 def view_of_card(rank, suit):
-    rank_emojis = {
-        2: "2️⃣",
-        3: "3️⃣",
-        4: "4️⃣",
-        5: "5️⃣",
-        6: "6️⃣",
-        7: "7️⃣",
-        8: "8️⃣",
-        9: "9️⃣",
-        10: "🔟",
-        11: "🅹",
-        12: "🆀",
-        13: "🅺",
-        14: "🅰"
-    }
-    suit_emojis = {
-        'Clubs': "\U00002663",
-        'Hearts': "\U00002665",
-        'Spades': "\U00002660",
-        'Diamonds': "\U00002666"
-    }
-    rank = rank_emojis.get(rank)
-    suit_with_emoji = suit_emojis.get(suit)
-    return rank + " " + suit_with_emoji
+    if suit == "Clubs":
+        if rank == 2:
+            return "CAACAgUAAxkBAAEJmx5kpebc_XlwoOaxJR7AXpOnMN9LjgAC7gADzD_AVUNl620pf85VLwQ"
+        elif rank == 3:
+            return "CAACAgUAAxkBAAEJmyBkpecF9ZOmrnEkjJSTYpndbhjp-QACxAADjXXJVSgF1c4SfQRVLwQ"
+        elif rank == 4:
+            return "CAACAgUAAxkBAAEJmyJkpecnUi0fkXBD2IPd0ajk9ProjQAC1QADdn3BVdsjMJCRrwM-LwQ"
+        elif rank == 5:
+            return "CAACAgUAAxkBAAEJmyRkpedGJAOB-Eb7mW_2ewlAEER6_gACQQEAAqyuwFUKLvZfnYor0C8E"
+        elif rank == 6:
+            return "CAACAgUAAxkBAAEJmyZkpedrVZtaKIWayjNNgY8OZYDL1wACLAEAAvSuwVXTZanNGx7wAi8E"
+        elif rank == 7:
+            return "CAACAgUAAxkBAAEJmyhkpeeOYeM-MW2LV73sKC92gUX2HAAC6QADmpzAVfqejKTvA09xLwQ"
+        elif rank == 8:
+            return "CAACAgUAAxkBAAEJmypkpeepvj0QZndAlV_mdXS-oeY7rwACZQEAAmsnwFXzDbUvNrlSIC8E"
+        elif rank == 9:
+            return "CAACAgUAAxkBAAEJmyxkpefM61NNGnMneYUjIgUYSHemHQACHwEAAoPTwVX0XbSdYZTway8E"
+        elif rank == 10:
+            return "CAACAgUAAxkBAAEJmy5kpefwbrb2TsRWyNEIJjutMODUEgACWAEAApCMwFVqmiojuRUZnS8E"
+        elif rank == 11:
+            return "CAACAgUAAxkBAAEJmzNkpegaKtyqghX1MSA3PoDjokOT7QACSgEAArVSwVWnJirONvGsjC8E"
+        elif rank == 12:
+            return "CAACAgUAAxkBAAEJmzVkpeg_F5oR74pLOEGeXqWpbPkKsQACYwEAAtZUwVVB9Jrr699DBi8E"
+        elif rank == 13:
+            return "CAACAgUAAxkBAAEJmzlkpehd3B_CsFR_HIsOBN7yIa4j6gACagEAArV1wFUsUwIdrbHrzi8E"
+        else:
+            return "CAACAgUAAxkBAAEJmztkpeiFpPsecJVyzK2mYSo9KoweagACVgEAAk7KwFUdsPO8bDy3zS8E"
+    elif suit == "Hearts":
+        if rank == 2:
+            return "CAACAgUAAxkBAAEJm0JkpejBZ7Ps5jtZ_llkr3xUkyMcxAACwAEAAri0wFUoQPrcy55z9S8E"
+        elif rank == 3:
+            return "CAACAgUAAxkBAAEJm0RkpejjohHf8SAdYPZRw3YSNsG2WQACkgEAAi3FwVUyj7OilKkEQi8E"
+        elif rank == 4:
+            return "CAACAgUAAxkBAAEJm0hkpelZdPWuygoUMEy383WahLC8IAACnQEAAtIewFXlnnP4enKOES8E"
+        elif rank == 5:
+            return "CAACAgUAAxkBAAEJm0pkpelsKmtgxIAPzslWNHpurml6rQAC9wADiiHBVSWGu92DeH5pLwQ"
+        elif rank == 6:
+            return "CAACAgUAAxkBAAEJm0xkpel-GfQx7eK0qTW0SuTYDfW38AACuQEAAi7fwVUndFyOv1LeuS8E"
+        elif rank == 7:
+            return "CAACAgUAAxkBAAEJm05kpemOevuEQF0HotvlyRSCdAgYvAACZAEAAtV9wVVc2N-IuRlx0C8E"
+        elif rank == 8:
+            return "CAACAgUAAxkBAAEJm1BkpemZd1mAY0eOeCsCBR4b044s-QACWAEAAk-6wVUC0fBkmnNZvC8E"
+        elif rank == 9:
+            return "CAACAgUAAxkBAAEJm1JkpemkAnR8l-8s3OmCHL_TvgObjQACpAEAAoDywVXcV-kFLl4Goy8E"
+        elif rank == 10:
+            return "CAACAgUAAxkBAAEJm1RkpemyyecueMlXEPw5TDtCODL_DwACEQEAAkFbwFVuiN9FfpNZWS8E"
+        elif rank == 11:
+            return "CAACAgUAAxkBAAEJm1hkpenCAa-7Ny7MgmEbEhfMGy56GQACTwEAAuZ6wVVeHVQy8OtDZC8E"
+        elif rank == 12:
+            return "CAACAgUAAxkBAAEJm1Zkpem-Tsfs-zpYiuh3UXCKY4YUfwACVwEAAhgewFWcKPy_MO0FAi8E"
+        elif rank == 13:
+            return "CAACAgUAAxkBAAEJm1pkpenfmKP1bc0YZsq0I4FtyIrIlgACGQEAAhRTwVWjUBF47pA6ES8E"
+        else:
+            return "CAACAgUAAxkBAAEJm1xkpenriYn8gyGYgWl-lMvI2F5S0QACigEAAvSIwFVK_o4a4DzQaC8E"
+    elif suit == "Spades":
+        if rank == 2:
+            return "CAACAgUAAxkBAAEJm15kpeuu42bO_JqrYPLjadF08QABtgUAAnYBAALPscBVqkQzr6Yem3ovBA"
+        elif rank == 3:
+            return "CAACAgUAAxkBAAEJm2Bkpeu5g-VGsCgYDhLjjyd6Dux-HwACiAEAAlr1wFXDzixQPWB0ay8E"
+        elif rank == 4:
+            return "CAACAgUAAxkBAAEJm2JkpevDdPKZxKxqgVkNJzmRQIjybAAC9QADkD_AVTCXjNsUgWFcLwQ"
+        elif rank == 5:
+            return "CAACAgUAAxkBAAEJm2RkpevOvtxcm6mTkVpRySZZRTsFhwACtgEAAnl5wVUt6BMU1V4rQS8E"
+        elif rank == 6:
+            return "CAACAgUAAxkBAAEJm2ZkpevYV40CGjF0yRyAx-CJoU2rTgACwgEAAhMZyFUVAQk2w9IUoy8E"
+        elif rank == 7:
+            return "CAACAgUAAxkBAAEJm2hkpevkc9gbeZecNow13mgMZm_XRgACXwEAAlcqwVWYKA6AaDeLNC8E"
+        elif rank == 8:
+            return "CAACAgUAAxkBAAEJm2pkpevuZCRgs3IuRWmGNCt2QQfaSQAC5gEAApGAwFVYXzYRg5l2hS8E"
+        elif rank == 9:
+            return "CAACAgUAAxkBAAEJm2xkpev7r506r0Ux3uKSe44kS9FaSAACTAEAAlTbwVUOieck2aRPdC8E"
+        elif rank == 10:
+            return "CAACAgUAAxkBAAEJm25kpewGDon8FBUcRD96bqP62NQvywACfgEAAjEccVarx0aBCU7B3C8E"
+        elif rank == 11:
+            return "CAACAgUAAxkBAAEJm3BkpewQJOW4JIu_ZaqpV5nIdG1cMgACgQEAAg1zwFVqLPJXSoY2rC8E"
+        elif rank == 12:
+            return "CAACAgUAAxkBAAEJm3JkpewZ5-8sI6HkgA9yHYwvLXN01QACqQIAAomTwFVYoAJh92UFLi8E"
+        elif rank == 13:
+            return "CAACAgUAAxkBAAEJm3RkpewjwYqjj3CkcuAXu84UGuoDEAACpQEAAmtpwFXgo7SNLAsgqC8E"
+        else:
+            return "CAACAgUAAxkBAAEJm3ZkpewsuZ1xIbwvTOUFK9xxZKfFlwACewEAAoCZwFXg-Nr-7hZaoC8E"
+    else:
+        if rank == 2:
+            return "CAACAgUAAxkBAAEJm3hkpew51Ld1sEqrbHrQYW2NH9wb9AACBgEAAmovyFWY6dj4c94fYi8E"
+        elif rank == 3:
+            return "CAACAgUAAxkBAAEJm3pkpexezDIYng-nOKmLxscC_Ha8cQAClQEAAuvlwVUAAfo2HFjA-HkvBA"
+        elif rank == 4:
+            return "CAACAgUAAxkBAAEJm3xkpexsg2H2fR8VUQZbVvEbdCLk2AACwQEAAqDNwFWiDdTqzKK29i8E"
+        elif rank == 5:
+            return "CAACAgUAAxkBAAEJm35kpex5YialkcnlbKEcY_WAPrLbTgACmQEAApEgwVUDWtfULkqa7i8E"
+        elif rank == 6:
+            return "CAACAgUAAxkBAAEJm4BkpeyFmiiiQk7cG63KIhJKbEfvdwACFwEAAvt_wVWgx6nKpXzQYi8E"
+        elif rank == 7:
+            return "CAACAgUAAxkBAAEJm4JkpeyOSUJSR8BTqwmkJgOf4WbX0AACjQEAArJWwFW0ccMe7TSgKi8E"
+        elif rank == 8:
+            return "CAACAgUAAxkBAAEJm4RkpeyZELqffzkbjp9eGLDD28QiVAACXQEAAmtEwFWjpcTqT8Nj4S8E"
+        elif rank == 9:
+            return "CAACAgUAAxkBAAEJm4ZkpeyjonqWXsIYxLrmpdnbZgOagwACNQEAAuQkwVVVg-Fo5xtXQi8E"
+        elif rank == 10:
+            return "CAACAgUAAxkBAAEJm4hkpeyugogPeik0CjELnKAcF7xpWgACUgEAAhlbwFV8m-J7WE21Ki8E"
+        elif rank == 11:
+            return "CAACAgUAAxkBAAEJm4pkpey57_Z1KYguCylkaT4h_NhGCgACTQEAAnHCyVW3tBaN5V6XEC8E"
+        elif rank == 12:
+            return "CAACAgUAAxkBAAEJm4xkpezHKv6OHQbB6E2l5B39Z-yC_gACuAEAArhlwFVXtlTiCEWnDS8E"
+        elif rank == 13:
+            return "CAACAgUAAxkBAAEJm45kpezRWxGpcgqY1BYRg5r6CihfkgACfgEAArn-wVWBh7R834WGzy8E"
+        else:
+            return "CAACAgUAAxkBAAEJm5BkpezbeDgmayw31whR3tGbXGNmdgACTAEAArlrwVUCjF9P-WDqbC8E"
 
 
 class Player:
@@ -112,8 +195,8 @@ class Player:
             if croupier_sum <= 16:
                 croupier_card = deck.give_user_card(self)
                 telegram_bot.send_message(message.chat.id, "=========================")
-                telegram_bot.send_message(message.chat.id, "Croupier next card is " + croupier_card)
-
+                telegram_bot.send_message(message.chat.id, "Croupier next card is ")
+                telegram_bot.send_sticker(message.chat.id, str(croupier_card))
             else:
                 telegram_bot.send_message(message.chat.id, "=========================")
                 telegram_bot.send_message(message.chat.id, "Croupier don't take a card ")
@@ -257,8 +340,11 @@ def play(message):
     print(list_of_players)
     deck.shuffle()
     deck.index = 0
+    if get_player(message.from_user.id).currency < 50:
+        telegram_bot.send_message(message.chat.id, "Your currency is lower than minimum bet")
+        return
     telegram_bot.send_message(message.chat.id, "Enter your bet:")
-    telegram_bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEJmxFkpePnsFLZl_STLAQga5CoLDr4IQAC7T4AApzT2UneXEXb-Tyadi8E')
+
 
 @telegram_bot.message_handler()
 def get_user_text(message):
@@ -268,12 +354,10 @@ def get_user_text(message):
     croupier = get_player(str(message.from_user.id) + message.from_user.username)
     result_of_game = ResultOfGame()
     print(player.currency)
-    if player.currency < 50:
-        telegram_bot.send_message(message.chat.id, "Your currency is lower than minimum bet")
-        return
     if message.text == "Hit":
         player_card = deck.give_user_card(player)
-        telegram_bot.send_message(message.chat.id, "Your next card is - " + player_card)
+        telegram_bot.send_message(message.chat.id, "Your next card is:")
+        telegram_bot.send_sticker(message.chat.id, str(player_card))
         player_sum = player.counting_sum()
 
         if player_sum > 21:
@@ -287,8 +371,6 @@ def get_user_text(message):
         while croupier_sum <= 16:
             croupier.croupier_choice(croupier_sum, message)
             croupier_sum = croupier.counting_sum()
-
-        telegram_bot.send_message(message.chat.id, "=========================")
         if croupier_sum == 21 and len(croupier.cards_on_hands) == 2:
             telegram_bot.send_message(message.chat.id, f'<b>Your final sum is - </b> {player_sum}',
                                       parse_mode='html')
@@ -300,7 +382,7 @@ def get_user_text(message):
                                   parse_mode='html')
 
         if croupier_sum > 21 >= player_sum or 21 >= player_sum > croupier_sum:
-            player.currency += player.bet*2
+            player.currency += player.bet * 2
             result_of_game.winner(message.chat.id)
         elif player_sum == croupier_sum:
             player.currency += player.bet
@@ -321,9 +403,12 @@ def get_user_text(message):
                                   reply_markup=markup)
     elif message.text == 'Your balance - ' + str(get_player(message.from_user.id).currency):
         telegram_bot.send_message(message.chat.id,
-                                  "5000 chips - 100 rubles\n for getting free chips give us A's for the PP course :)")
+                                  "5000 chips - 100 rubles\nFor getting free chips give us A's for the PP course :)")
     elif message.text.isdigit():
         player.bet = int(message.text)
+        if player.bet < 50:
+            telegram_bot.send_message(message.chat.id, "Your bet is not accepted!\nMinimum value of bet - 50")
+            return play(message)
         player.currency -= player.bet
 
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
@@ -332,14 +417,14 @@ def get_user_text(message):
         balance_button = types.InlineKeyboardButton('Your balance - ' + str(get_player(message.from_user.id).currency))
         markup.add(hit_button, stand_button, balance_button)
         telegram_bot.send_message(message.chat.id, "Your bet is accepted", reply_markup=markup)
-        telegram_bot.send_message(message.chat.id, "=========================")
         player_card = deck.give_user_card(player)
-        telegram_bot.send_message(message.chat.id, "Your first card is " + player_card)
+        telegram_bot.send_message(message.chat.id, "Your first cards are ")
+        telegram_bot.send_sticker(message.chat.id, str(player_card))
         player_card = deck.give_user_card(player)
-        telegram_bot.send_message(message.chat.id, "Your second card is " + player_card)
-        telegram_bot.send_message(message.chat.id, "=========================")
+        telegram_bot.send_sticker(message.chat.id, str(player_card))
         croupier_card = deck.give_user_card(croupier)
-        telegram_bot.send_message(message.chat.id, "Croupier first card is " + croupier_card)
+        telegram_bot.send_message(message.chat.id, "Croupier first card is ")
+        telegram_bot.send_sticker(message.chat.id, str(croupier_card))
 
         player_sum = player.counting_sum()
         if player_sum == 21:
@@ -349,7 +434,7 @@ def get_user_text(message):
                 player.currency += player.bet
                 result_of_game.draw(message.chat.id)
             else:
-                player.currency += (player.bet * 15)//10
+                player.currency += (player.bet * 15) // 10
                 result_of_game.blackjack(message.chat.id)
 
     else:
