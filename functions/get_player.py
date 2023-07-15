caf3561786d9788ceb69c6daa021a27657860605
Dzyuba Sergey <97405763+rationalf@users.noreply.github.com>
2023-07-15 20:10:07 +0300

    get_player.py

--- functions/get_player.py
+++ functions/get_player.py
@@ -1,4 +1,5 @@
 def get_player(id_of_user, list_of_players):
+    '''Function for getting player from list'''
     for player in list_of_players:
         if player.id == id_of_user:
             return player
