1d2dc38422db2ddbfda7b2b9baae0fb481de19f1
Dzyuba Sergey <97405763+rationalf@users.noreply.github.com>
2023-07-15 20:16:37 +0300

    play_command.py

--- Commads/play_command.py
+++ Commads/play_command.py
@@ -9,6 +9,7 @@ telegram_bot = telebot.TeleBot('6071571860:AAFch9-DHyN7EZ8zZUQRk5aM50u-ZD05cgs')
 
 @telegram_bot.message_handler(commands=['play'])
 def play_command(message, deck, list_of_players):
+    '''Creating a deck for gaming and starting game'''
     del deck
 
     def delete_player(id_of_user):
